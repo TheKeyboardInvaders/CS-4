45cd9b40094b8a9c483b2d2d49ebcfd39815bcc7
realAbstractionist <justforsignup@mail.ru>
2015-10-11 15:46:29 +0700

    Peter The First Encode/Decode. Todo list: tasks added.

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,9 @@ namespace CS_4
 {
     public partial class Form1 : Form
     {
+        private readonly Dictionary<char, string> encodeTable = new Dictionary<char, string>();
+        private readonly Dictionary<string, char> decodeTable;
+
         char[] alpharus = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
                             'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ',
                             'Ы', 'Ь', 'Э', 'Ю', 'Я', 'А', 'Б', 'В'
@@ -28,6 +31,44 @@ namespace CS_4
             cbAlphabet.Items.Add("Русский");
             cbAlphabet.Items.Add("Английский");
             cbAlphabet.SelectedItem = "Русский";
+
+            // todo: refactor with loop
+            // todo: correct the alphabet
+            // todo: consider space issue
+            encodeTable.Add('а', "ме");
+            encodeTable.Add('б', "ли");
+            encodeTable.Add('в', "ко");
+            encodeTable.Add('г', "ин");
+            encodeTable.Add('д', "зе");
+            encodeTable.Add('е', "жу");
+            encodeTable.Add('ж', "ню");
+            encodeTable.Add('и', "о");
+            encodeTable.Add('й', "пы");
+            encodeTable.Add('к', "ра");
+            encodeTable.Add('л', "су");
+            encodeTable.Add('м', "ти");
+            encodeTable.Add('н', "у");
+            encodeTable.Add('о', "хм");
+            encodeTable.Add('п', "от");
+            encodeTable.Add('р', "ца");
+            encodeTable.Add('с', "чу");
+            encodeTable.Add('т', "ше");
+            encodeTable.Add('у', "ам");
+            encodeTable.Add('ф', "э");
+            encodeTable.Add('х', "ъ");
+            //encodeTable.Add('ы', "от");
+            encodeTable.Add('ц', "ь");
+            //encodeTable.Add('ч', "ъ");
+            encodeTable.Add('ш', "ю");
+            encodeTable.Add('щ', "я");
+            encodeTable.Add('ъ', "ф");
+            encodeTable.Add('ь', "бе");
+            encodeTable.Add('э', "ва");
+            encodeTable.Add('ю', "гу");
+            encodeTable.Add('я', "дм");
+
+            // Меняем ключ-значение для таблицы расшифровки
+            decodeTable = encodeTable.ToDictionary(kv => kv.Value, kv => kv.Key);
         }
 
         #region Шифровка текста кодом Цезаря
@@ -98,7 +139,15 @@ namespace CS_4
 
         private void PetrIEncode(char[] alphabet)
         {
-            
+            string input = tbInput.Text.ToLower();
+            string output = "";
+
+            foreach (Char c in input)
+            {
+                output += encodeTable[c];
+            }
+
+            tbResult.Text = output;
         }
         #endregion
 
@@ -117,7 +166,23 @@ namespace CS_4
 
         private void PetrIDecode(char[] alphabet)
         {
+            string input = tbInput.Text;
+            string output = "";
 
+            string tmp = "";
+            for (int i = 0; i < input.Length - 1; ++i)
+            {
+                tmp = input[i].ToString() + input[i + 1].ToString();
+                if (decodeTable.ContainsKey(tmp))
+                {
+                    output += decodeTable[tmp];
+                    tmp = "";
+                }
+                else if (decodeTable.ContainsKey(input[i].ToString()))
+                    output += decodeTable[input[i].ToString()];
+            }
+
+            tbResult.Text = output;
         }
         #endregion
 
